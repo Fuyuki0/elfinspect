60e57535de329770dffb77fbf95d39b5abafa5ec
Fuyuki0 <pptreenuson1710@gmail.com>
2026-01-03 21:15:57 +0700

    adding include

---
+++ Makefile
@@ -0,0 +1,12 @@
+CC=gcc
+CFLAGS=-Wall -Wextra -g
+TARGET=elfinspect
+
+SRC=src/main.c src/elf_loader.c
+INC=-Iinclude
+
+all:
+	$(CC) $(CFLAGS) $(SRC) $(INC) -o $(TARGET)
+
+clean:
+	rm -f $(TARGET)
---
+++ src/elf_loader.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include "elf_loader.h"
+
+int load_elf(const char* filename) {
+	FILE* fp = fopen(filename, "rb");
+	if (!fp) {
+		perror("fopen");
+		return -1;
+	}
+
+	Elf64_Ehdr ehdr;
+	fread(&ehdr, 1, sizeof(ehdr), fp);
+
+	if (ehdr.e_ident[0] != 0x7f ||
+		ehdr.e_ident[1] != 'E' ||
+		ehdr.e_ident[2] != 'L' ||
+		ehdr.e_ident[3] != 'F' ||
+	   ) {
+		printf("Not an ELF file\n");
+		fclose(fp);
+		return -1;
+	}
+
+	printf("ELF Header: \n");
+
+	printf("	Class: %s\n", ehdr.e_indent[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("	Endianness: %s\n", ehdr.e_indent[EI_DATA] == ELFCLASS64 ? "Little Endian" : "Big Endian");
+
+	printf("	Entry point: 0x%lx\n", ehdr.e_entry);
+
+	printf("	Machine: ");
+	switch (ehdr.e_machine) {
+		case EM_X86_64: printf("x86-64\n"); break;
+		case EM_386: printf("x86\n"); break;
+		case EM_ARM: printf("ARM\n"); break;
+		default: printf("Unknown (%d)\n", ehdr.e_machine);
+	}
+	
+	fclose(fp);
+	return 0;
+}
